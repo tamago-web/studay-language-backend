334e2e51c5c91a7405963892016c92a5a69bf7bc
kana miyazaki <>
2024-09-23 23:56:43 +0900

    確認

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/check-env', function () {
-    return response()->json(['frontend_url' => env('FRONTEND_URL')]);
-});
